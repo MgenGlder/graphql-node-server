be9b49756fd197a7e0cc38c63eadbd21ba086bbf
MgenGlder <mgen.glder@gmail.com>
2023-06-01 23:47:26 -0400

    Add wildcard matcher for resolver composition

--- server.js
+++ server.js
@@ -187,7 +187,7 @@ const hasRole = (role) => next => (root, args, context, info) => {
 }
 
 const resolversComposition = {
-    'Query.warriors': [isAuthenticated(), hasRole('EDITOR')]
+    'Query.*': [isAuthenticated(), hasRole('EDITOR')]
 }
 
 const composeResolvers = composeResolvers(resolvers, resolversComposition);
